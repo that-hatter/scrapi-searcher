6ef652e24a586530bf1c288eda87e4dc35d3b138
Hatter <47074795+that-hatter@users.noreply.github.com>
2025-02-15 16:06:25 +0800

    fix message edit handling
    used to not send a new response if there was no previous response to delete

--- src/lib/events/messageUpdate.ts
+++ src/lib/events/messageUpdate.ts
@@ -1,4 +1,4 @@
-import { pipe, RTE } from '@that-hatter/scrapi-factory/fp';
+import { RTE } from '@that-hatter/scrapi-factory/fp';
 import type { Event } from '../modules';
 import { messageCreate } from './messageCreate';
 import { messageDelete } from './messageDelete';
@@ -6,8 +6,8 @@ import { messageDelete } from './messageDelete';
 export const messageUpdate: Event.Event<'messageUpdate'> = {
   name: 'messageUpdate',
   handle: (message) =>
-    pipe(
+    RTE.sequenceArray([
       messageDelete.handle(message),
-      RTE.flatMap(() => messageCreate.handle(message))
-    ),
+      messageCreate.handle(message),
+    ]),
 };
